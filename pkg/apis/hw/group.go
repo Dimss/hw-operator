343fe17d55b481cc1010e0f0e286832e12b832c5
Dmitry Kartsev <dkartsev@redhat.com>
2019-11-05 21:42:57 +0200

    first commit

---
+++ pkg/apis/hw/group.go
@@ -0,0 +1,6 @@
+// Package hw contains hw API versions.
+//
+// This file ensures Go source parsers acknowledge the hw package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package hw
